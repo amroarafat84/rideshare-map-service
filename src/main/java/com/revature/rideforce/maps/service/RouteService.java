265f2bdc1bde884017de2a58377a331ca869c218
Jonathan Novoa <jon142536@gmail.com>
2018-10-09 09:57:21 -0400

    index file errors attempt 1

---
+++ src/main/java/com/revature/rideforce/maps/service/RouteService.java
@@ -0,0 +1,75 @@
+package com.revature.rideforce.maps.service;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.TravelMode;
+import com.revature.rideforce.maps.beans.Route;
+
+/**
+ * The route service
+ * @author Revature Java batch
+ * @Component
+ */
+@Component
+public class RouteService {
+	private static final Logger log = LoggerFactory.getLogger(RouteService.class);
+
+  	/**
+  	 * Injecting the GeoApiContext
+  	 */
+	@Autowired
+	private GeoApiContext geoApiContext;
+
+	public GeoApiContext getGeoApiContext() {
+		return geoApiContext;
+	}
+
+	public void setGeoApiContext(GeoApiContext geoApiContext) {
+		this.geoApiContext = geoApiContext;
+	}
+
+	public RouteService(GeoApiContext geoApiContext) {
+		super();
+		this.geoApiContext = geoApiContext;
+	}
+
+	public RouteService() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * get the route
+	 * @param origin
+	 * @param destination
+	 * @return Route
+	 */
+	public Route getRoute(String origin, String destination) {
+		try {
+			DirectionsRoute route = DirectionsApi.getDirections(geoApiContext, origin, destination)
+					.mode(TravelMode.DRIVING).await().routes[0];
+			long distance = 0;
+			long duration = 0;
+
+			for (DirectionsLeg leg : route.legs) {
+				distance += leg.distance.inMeters;
+				duration += leg.duration.inSeconds;
+			}
+			return new Route(distance, duration);
+		} catch (ApiException | InterruptedException | IOException e) {
+			log.error("Unexpected exception when fetching route.", e);
+			Thread.currentThread().interrupt();
+			return null;
+		}
+	}
+}
